590bf50afd090ce9af7b2decd4eb18b71faac8a1
Pranav <pranavkrishnap856@gmail.com>
2024-11-10 13:25:54 +0530

    home button issue on AllTestsPage resolved

--- UI/AllTestsPage.py
+++ UI/AllTestsPage.py
@@ -30,7 +30,7 @@ class SideFrame(ctk.CTkFrame):
         self.btnHome=ctk.CTkButton(self,text='Home',command=self.master.master.LoadHomePage)
         self.btnHome.pack(side=ctk.BOTTOM)
         
-        self.btnBack=ctk.CTkButton(self,text='Back',command=home.LoadHomePage)
+        self.btnBack=ctk.CTkButton(self,text='Back',command=self.master.master.LoadHomePage)
         self.btnBack.pack(side=ctk.BOTTOM)
         
 class AllTestsFrame(ctk.CTkFrame):
